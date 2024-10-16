b944389ffeda91386508f5ac59df9413d39eda8c
ganeshchikati <gg3289963@gmail.com>
2024-10-16 19:43:36 +0530

    add guessanumber.cpp via upload

---
+++ guessanumber.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+    srand((unsigned int) time(NULL));
+ 
+    int number = (rand() % 100) +1;
+    int guess = 0;
+    
+    do
+    {
+
+    cout << "Enter Guess (1-100) :" ;
+    cin  >> guess;
+
+    if (guess > number)
+    cout << "Guess lower!" <<endl;
+
+    else if (guess < number)
+    cout << "Guess higher!" <<endl;
+
+    else
+    cout << "you won!" <<endl;
+
+
+    }while (guess != number );
+        
+    return 0;
+}
